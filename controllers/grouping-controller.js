a22ee37586d6c162d4f9dbac81910148ec0553fa
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-04-30 16:03:51 +0500

    Add groupingCreatePost controller & its route

--- controllers/grouping-controller.js
+++ controllers/grouping-controller.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require('express-validator');
 const Grouping = require('../models/grouping');
 const Country = require('../models/country');
 
@@ -33,6 +34,55 @@ const groupingCreateGet = (req, res, next) => {
   });
 };
 
-const groupingController = { groupingsList, groupingDetail, groupingCreateGet };
+const groupingCreatePost = [
+  body('name').trim().escape(),
+  body('shortName').trim().escape(),
+  body('description').trim().escape(),
+  body('country.*').escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const grouping = new Grouping({
+      name: req.body.name,
+      shortName: req.body.shortName,
+      description: req.body.description,
+      countries: req.body.country,
+    });
+
+    if (!errors.isEmpty()) {
+      Country.find({}).exec((err, result) => {
+        if (err) return next(err);
+        for (let i = 0; i < result.length; i += 1) {
+          if (grouping.countries.indexOf(result[i]._id) > -1) {
+            /* eslint-disable no-param-reassign */
+            result[i].checked = true;
+          }
+        }
+        return res.render('grouping-form', {
+          title: 'Create a Grouping',
+          countries: result,
+          name: grouping.name,
+          shortName: grouping.shortName,
+          description: grouping.description,
+        });
+      });
+    }
+
+    Grouping.findOne({ name: grouping.name }).exec((err, result) => {
+      if (err) return next(err);
+      if (result) return res.redirect(result.url);
+      return grouping.save((error) => {
+        if (error) return next(error);
+        return res.redirect(grouping.url);
+      });
+    });
+  },
+];
+
+const groupingController = {
+  groupingsList,
+  groupingDetail,
+  groupingCreateGet,
+  groupingCreatePost,
+};
 
 module.exports = groupingController;
--- routes/grouping-router.js
+++ routes/grouping-router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const {
   groupingDetail,
   groupingCreateGet,
+  groupingCreatePost,
 } = require('../controllers/grouping-controller');
 
 const router = express.Router();
 
 router.get('/create', groupingCreateGet);
+router.post('/create', groupingCreatePost);
 router.get('/:id', groupingDetail);
 
 module.exports = router;
